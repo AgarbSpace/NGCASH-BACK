4935b717c70f6817dc96752938828540d925a18b
Luis Henrique <luishenrinquebraga@gmail.com>
2022-11-19 15:18:07 -0300

    fix: Errors sent texts changes

--- src/middlewares/validateSchemaMiddleware.ts
+++ src/middlewares/validateSchemaMiddleware.ts
@@ -7,8 +7,8 @@ export default function validateSchemaMiddleware(schema: any) {
     if (validation.error) {
       const error = validation.error.details.map((validationError: any) => validationError.message);
       const { value } = validation.error.details[0].context;
-      if (error[0] === `"password" with value "${value}" fails to match the required pattern: /^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/`) {
-        error[0] = 'Password must contain at least one capital letter, one number and 8 characters!!';
+      if (error[0] === `"password" with value "${value}" fails to match the required pattern: /^(?=.*\\d)(?=.*[A-Z])[0-9a-zA-Z]{8,}$/`) {
+        error[0] = 'A senha deve conter ao menos 8, uma Letra maiúscula e um número!!';
       }
       return response.status(422).send(error);
     }
--- src/schemas/signUpFormSchema.ts
+++ src/schemas/signUpFormSchema.ts
@@ -2,7 +2,7 @@ import joi from 'joi';
 
 const signUpFormSchema = joi.object({
   username: joi.string().min(3).required(),
-  password: joi.string().regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/).required(),
+  password: joi.string().regex(/^(?=.*\d)(?=.*[A-Z])[0-9a-zA-Z]{8,}$/).required(),
 });
 
 export default signUpFormSchema;
